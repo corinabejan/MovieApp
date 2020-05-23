6d4d9805a34eafa3fa695d9a390c2f72603532b8
corina <corinabe93@gmail.com>
2020-05-23 19:00:32 +0200

    more information added

--- src/pages/Popup.js
+++ src/pages/Popup.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function Popup({ selected, closePopup }) {
-	return (
-		<section className="popup">
-			<div className="content">
-				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
-				<p className="rating">Rating: {selected.imdbRating}</p>
-				<div className="plot">
-					<img src={selected.Poster} />
-					<p>{selected.Plot}</p>
-				</div>
-				<button className="close" onClick={closePopup}>Close</button>
-			</div>
-		</section>
-	)
+  return (
+    <section className="popup">
+      <div className="content">
+        <h2>
+          {selected.Title} <span>({selected.Year})</span>
+        </h2>
+        <p className="rating">Rating: {selected.imdbRating}</p>
+        <div className="plot">
+          <img src={selected.Poster} />
+          <p>{selected.Plot}</p>
+          <p>Director: {selected.Director}</p>
+          <p>Actors: {selected.Actors}</p>
+          <p>Awards: {selected.Awards}</p>
+          <p>Genre: {selected.Genre}</p>
+          <p>Runtime: {selected.Runtime}</p>
+        </div>
+        <button className="close" onClick={closePopup}>
+          Close
+        </button>
+      </div>
+    </section>
+  );
 }
-
